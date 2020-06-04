b07858224287a92c4a6de7099c5042f638029e51
Hergoln <juliusz.malka@o2.pl>
2020-06-04 11:53:53 +0200

    feature1_impl implementacja kroku "Wtedy"

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -1,32 +1,41 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
-import java.util.List;
-
-import org.joda.time.LocalTime;
-
 import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.TimetableService;
+import org.hamcrest.Matchers;
+import org.joda.time.LocalTime;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
 
 public class OptimalItinerarySteps {
 
+    private Line line;
+    private String lineStart, departure, destination;
+    private TimetableService timetableService;
+
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
-            @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
+                                    @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
         throw new PendingException();
-
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination,
-            @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
+                                  @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
         throw new PendingException();
     }
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        throw new PendingException();
+        List<LocalTime> got = timetableService.findArrivalTimes(this.line, destination);
+
+        got.retainAll(expectedTrainTimes);
+        assertThat(got.size(), Matchers.equalTo(expectedTrainTimes.size()));
     }
 }
