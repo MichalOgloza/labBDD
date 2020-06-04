79d5997b6f066f267790eb4c536c868723479739
Hergoln <juliusz.malka@o2.pl>
2020-06-04 12:19:38 +0200

    feature1_impl dodano interface IntineraryService oraz dodano zawartość kroku Gdy

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -5,19 +5,18 @@ import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
-import edu.iis.mto.bdd.trains.model.Line;
-import edu.iis.mto.bdd.trains.services.TimetableService;
+import edu.iis.mto.bdd.trains.services.IntineraryService;
 import org.hamcrest.Matchers;
 import org.joda.time.LocalTime;
+
 import java.util.List;
 
 import static org.junit.Assert.assertThat;
 
 public class OptimalItinerarySteps {
 
-    private Line line;
-    private String lineStart, departure, destination;
-    private TimetableService timetableService;
+    private IntineraryService intineraryService;
+    private List<LocalTime> got;
 
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
@@ -28,14 +27,12 @@ public class OptimalItinerarySteps {
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
     public void whenIWantToTravel(String departure, String destination,
                                   @Transform(JodaLocalTimeConverter.class) LocalTime startTime) {
-        throw new PendingException();
+        this.got = intineraryService.findNextDepartures(departure, destination, startTime);
     }
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        List<LocalTime> got = timetableService.findArrivalTimes(this.line, destination);
-
-        got.retainAll(expectedTrainTimes);
-        assertThat(got.size(), Matchers.equalTo(expectedTrainTimes.size()));
+        this.got.retainAll(expectedTrainTimes);
+        assertThat(this.got.size(), Matchers.equalTo(expectedTrainTimes.size()));
     }
 }
