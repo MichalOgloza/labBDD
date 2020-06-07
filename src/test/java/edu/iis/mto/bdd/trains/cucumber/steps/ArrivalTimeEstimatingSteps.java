ea673f9a34812b50ec61bd4dd3e65275b7d6abd5
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-07 19:49:37 +0200

    feature2_rev1 added automatically the rest of steps, but they are in the raw form

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeEstimatingSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeEstimatingSteps.java
@@ -1,11 +1,27 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.pl.Gdy;
+import cucumber.api.java.pl.I;
+import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładającże;
 
 public class ArrivalTimeEstimatingSteps {
-    @Zakładającże("^chcę się dostać z \"(.*)\" do \"(.*)\"")
-    public void givenStartAndDestination(String from, String to){
+
+    @Zakładającże("^chcę się dostać z (.*) do (.*)$")
+    public void givenStartAndDestination(String from, String to) {
         throw new PendingException();
+    }
+
+    @I("^następny pociąg odjeżdża o (\\d+):(\\d+) na linii Western$")
+    public void następnyPociągOdjeżdżaONaLiniiWestern(int arg0, int arg1) {
+    }
+
+    @Gdy("^zapytam o godzinę przyjazdu$")
+    public void zapytamOGodzinęPrzyjazdu() {
+    }
+
+    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (\\d+):(\\d+)$")
+    public void powinienemUzyskaćNastępującySzacowanyCzasPrzyjazdu(int arg0, int arg1) {
     }
 }
