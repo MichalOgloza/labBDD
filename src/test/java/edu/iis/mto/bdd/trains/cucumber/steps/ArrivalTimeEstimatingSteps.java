ccef2c201663c6bc471dc632bffd615277e2b549
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-07 19:43:12 +0200

    feature2_rev1 added precondition for ArrivalTimeEstimatingSteps.

---
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalTimeEstimatingSteps.java
@@ -0,0 +1,11 @@
+package edu.iis.mto.bdd.trains.cucumber.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.pl.Zakładającże;
+
+public class ArrivalTimeEstimatingSteps {
+    @Zakładającże("^chcę się dostać z \"(.*)\" do \"(.*)\"")
+    public void givenStartAndDestination(String from, String to){
+        throw new PendingException();
+    }
+}
