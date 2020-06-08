db7e69b8d6120c981ef7c1b5102ae67c8f08c4fe
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 03:37:55 +0200

    feature1_impl I decided to prepare also @given step of OptimalItinerarySteps before I define functionality with unit testing of future ItineraryService, moreover added temporarily checking context of ItineraryService.

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,8 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -10,19 +12,31 @@ import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class OptimalItinerarySteps {
 
     private List<LocalTime> arrivalTimes;
     private ItineraryService itineraryService;
-    
+
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-        throw new PendingException();
+
+        var trainLine = Line.named(line).departingFrom(lineStart);
+        var timeTableService = new InMemoryTimetableService();
+        itineraryService = StandardItineraryService.builder()
+                .ofLine(trainLine)
+                .withService(timeTableService)
+                .withInterval(15)
+                .build();
+
+        //is context working?
+        var computedDepartureTimes = timeTableService.findArrivalTimes(trainLine, departure);
+        assertThat(computedDepartureTimes, is(equalTo(departureTimes)));
+        var linesCollection = timeTableService.findLinesThrough(departure, destination);
+        assertThat(linesCollection, hasItem(trainLine));
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
