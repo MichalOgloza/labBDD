4d16ee2ac81bb3c4d8e132a1f623876667442627
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 14:46:04 +0200

    feature1_impl Added missing methods of ItineraryServiceBuilder, the behavior of StandardItineraryService methods will be determined after unit tests implementation.

--- src/main/java/edu/iis/mto/bdd/trains/services/StandardItineraryService.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/StandardItineraryService.java
@@ -1,9 +1,17 @@
 package edu.iis.mto.bdd.trains.services;
 
 import edu.iis.mto.bdd.trains.utils.ItineraryServiceBuilder;
+import org.joda.time.LocalTime;
 
-public class StandardItineraryService {
+import java.util.List;
+
+public class StandardItineraryService implements ItineraryService {
     public static ItineraryServiceBuilder builder() {
         return new ItineraryServiceBuilder();
+    }
+
+    @Override
+    public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime) {
+        return null;
     }
 }
--- src/main/java/edu/iis/mto/bdd/trains/utils/ItineraryServiceBuilder.java
+++ src/main/java/edu/iis/mto/bdd/trains/utils/ItineraryServiceBuilder.java
@@ -1,11 +1,25 @@
 package edu.iis.mto.bdd.trains.utils;
 
+import edu.iis.mto.bdd.trains.model.Line;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import edu.iis.mto.bdd.trains.services.StandardItineraryService;
 
 public class ItineraryServiceBuilder implements Builder<StandardItineraryService> {
-    
+
     @Override
     public StandardItineraryService build() {
         return new StandardItineraryService();
+    }
+
+    public ItineraryServiceBuilder ofLine(Line trainLine) {
+        return this;
+    }
+
+    public ItineraryServiceBuilder withTimeTableService(InMemoryTimetableService timeTableService) {
+        return this;
+    }
+
+    public ItineraryServiceBuilder withInterval(int i) {
+        return this;
     }
 }
