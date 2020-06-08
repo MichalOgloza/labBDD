0386d0b983f07de109be8557c5defb6cb4fe89c7
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 16:15:37 +0200

    feature1_impl added behavior tests that are scratch for implementation of ItineraryService method findNextDepartures.

--- src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimesTest.java
+++ src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimesTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.bdd.trains.junit;
 import edu.iis.mto.bdd.trains.model.Line;
 import edu.iis.mto.bdd.trains.services.ItineraryService;
 import edu.iis.mto.bdd.trains.services.TimetableService;
+import org.junit.Test;
 import org.mockito.Mock;
 
 public class WhenCalculatingArrivalTimesTest {
@@ -11,5 +12,29 @@ public class WhenCalculatingArrivalTimesTest {
     @Mock
     private TimetableService timetableService;
     private Line testedLine;
-    
+
+    @Test
+    public void passingNegativeIntervalToItineraryServiceShouldCauseIllegalArgumentException() {
+        
+    }
+
+    @Test
+    public void passingNullTimeTableServiceToItineraryServiceShouldEndUpWithNPEAfterCallingFindNextDepartures() {
+        
+    }
+
+    @Test
+    public void ifTimetableServiceInjectedToItineraryServiceReturnedNullOrEmptyListOfArrivalTimesThenCallingFindNextDeparturesShouldEndUpWithNPE() {
+        
+    }
+
+    @Test
+    public void ifBetweenProvidedDepartureStationAndDestinationDoesNotExistAnyLineThenCallingFindNextDeparturesShouldEndUpIllegalArgumentExceptionThrown() {
+
+    }
+
+    @Test
+    public void findArrivalTimesOfTimetableServiceShouldBeCalledOncePerFindNextDeparturesCall() {
+
+    }
 }
