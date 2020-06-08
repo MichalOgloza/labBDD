e63bf21b2463159cfb8cdeb337b38fd6e0a6ddd9
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 02:56:01 +0200

    feature1_impl added @then step implementation, it obviously fails.

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -2,6 +2,7 @@ package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import java.util.List;
 
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import org.joda.time.LocalTime;
 
 import cucumber.api.PendingException;
@@ -10,8 +11,14 @@ import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class OptimalItinerarySteps {
 
+    private List<LocalTime> arrivalTimes;
+    
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
@@ -27,6 +34,6 @@ public class OptimalItinerarySteps {
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        throw new PendingException();
+        assertThat(arrivalTimes, is(equalTo(expectedTrainTimes)));
     }
 }
