cc2e204a5c66827ad31a53279334340ec3877f5e
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 14:54:32 +0200

    feature1_impl updated constructors and methods of ItineraryServiceBuilder

--- src/main/java/edu/iis/mto/bdd/trains/services/StandardItineraryService.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/StandardItineraryService.java
@@ -1,11 +1,23 @@
 package edu.iis.mto.bdd.trains.services;
 
+import edu.iis.mto.bdd.trains.model.Line;
 import edu.iis.mto.bdd.trains.utils.ItineraryServiceBuilder;
 import org.joda.time.LocalTime;
 
 import java.util.List;
 
 public class StandardItineraryService implements ItineraryService {
+
+    private final Line trainLine;
+    private final TimetableService timeTableService;
+    private final int minutes;
+
+    public StandardItineraryService(Line trainLine, TimetableService timeTableService, int minutes) {
+        this.trainLine = trainLine;
+        this.timeTableService = timeTableService;
+        this.minutes = minutes;
+    }
+
     public static ItineraryServiceBuilder builder() {
         return new ItineraryServiceBuilder();
     }
--- src/main/java/edu/iis/mto/bdd/trains/utils/ItineraryServiceBuilder.java
+++ src/main/java/edu/iis/mto/bdd/trains/utils/ItineraryServiceBuilder.java
@@ -1,25 +1,32 @@
 package edu.iis.mto.bdd.trains.utils;
 
 import edu.iis.mto.bdd.trains.model.Line;
-import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
 import edu.iis.mto.bdd.trains.services.StandardItineraryService;
+import edu.iis.mto.bdd.trains.services.TimetableService;
 
 public class ItineraryServiceBuilder implements Builder<StandardItineraryService> {
 
+    private int minutes;
+    private TimetableService timeTableService;
+    private Line trainLine;
+
     @Override
     public StandardItineraryService build() {
-        return new StandardItineraryService();
+        return new StandardItineraryService(trainLine, timeTableService, minutes);
     }
 
     public ItineraryServiceBuilder ofLine(Line trainLine) {
+        this.trainLine = trainLine;
         return this;
     }
 
-    public ItineraryServiceBuilder withTimeTableService(InMemoryTimetableService timeTableService) {
+    public ItineraryServiceBuilder withTimeTableService(TimetableService timeTableService) {
+        this.timeTableService = timeTableService;
         return this;
     }
 
-    public ItineraryServiceBuilder withInterval(int i) {
+    public ItineraryServiceBuilder withInterval(int minutes) {
+        this.minutes = minutes;
         return this;
     }
 }
