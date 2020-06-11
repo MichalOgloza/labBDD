49af46e83ca3a33c6ab01cd97c8573be01147f4c
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-11 11:42:47 +0200

    Implemented new test cases, added exampleArrivalTimes method

--- src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimesTest.java
+++ src/test/java/edu/iis/mto/bdd/trains/junit/WhenCalculatingArrivalTimesTest.java
@@ -2,24 +2,25 @@ package edu.iis.mto.bdd.trains.junit;
 
 import edu.iis.mto.bdd.trains.cucumber.steps.JodaLocalTimeConverter;
 import edu.iis.mto.bdd.trains.model.Line;
-import edu.iis.mto.bdd.trains.services.ItineraryService;
 import edu.iis.mto.bdd.trains.services.StandardItineraryService;
 import edu.iis.mto.bdd.trains.services.TimetableService;
 
+import org.joda.time.LocalTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.function.Executable;
-import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.mockito.Mockito.when;
 
 public class WhenCalculatingArrivalTimesTest {
     private final int STANDARD_INTERVAL = 15;
     private final int EXTENDED_INTERVAL = 30;
     private final int NEGATIVE_INTERVAL = -1;
 
-    @Mock
     private TimetableService timetableService;
 
     private Line testedLine;
@@ -29,6 +30,7 @@ public class WhenCalculatingArrivalTimesTest {
     public void setUp() throws Exception {
         testedLine = Line.named("Western").departingFrom("North Richmond");
         converter = new JodaLocalTimeConverter();
+        timetableService = Mockito.mock(TimetableService.class);
     }
 
     @Test
@@ -51,17 +53,40 @@ public class WhenCalculatingArrivalTimesTest {
     }
 
     @Test
-    public void ifTimetableServiceInjectedToItineraryServiceReturnedNullOrEmptyListOfArrivalTimesThenCallingFindNextDeparturesShouldEndUpWithNPE() {
+    public void ifTimetableServiceInjectedToItineraryServiceReturnedNullListOfArrivalTimesThenCallingFindNextDeparturesShouldEndUpWithNPE() {
+        when(timetableService.findArrivalTimes(Mockito.any(Line.class), Mockito.any())).thenReturn(null);
+        when(timetableService.findLinesThrough(Mockito.anyString(), Mockito.anyString())).thenReturn(Collections.singletonList(testedLine));
+        var itineraryService = StandardItineraryService.builder()
+                .withInterval(STANDARD_INTERVAL)
+                .withTimeTableService(timetableService)
+                .ofLine(testedLine).build();
 
+        Executable invalidCall = () -> itineraryService
+                .findNextDepartures("North Richmond", "Parramatta", converter.transform("08:29"));
+
+        assertThrows(NullPointerException.class, invalidCall);
     }
 
     @Test
     public void ifBetweenProvidedDepartureStationAndDestinationDoesNotExistAnyLineThenCallingFindNextDeparturesShouldEndUpIllegalArgumentExceptionThrown() {
+        when(timetableService.findArrivalTimes(Mockito.any(Line.class), Mockito.any())).thenReturn(exampleArrivalTimes());
+        var itineraryService = StandardItineraryService.builder()
+                .withInterval(STANDARD_INTERVAL)
+                .withTimeTableService(timetableService)
+                .ofLine(testedLine).build();
 
+        Executable invalidCall = () -> itineraryService
+                .findNextDepartures("ThisPlaceDoesNotExist", "ThisPlaceDoesNotExistToo", converter.transform("08:29"));
+
+        assertThrows(IllegalArgumentException.class, invalidCall);
     }
 
     @Test
     public void findArrivalTimesOfTimetableServiceShouldBeCalledOncePerFindNextDeparturesCall() {
 
+    }
+
+    private List<LocalTime> exampleArrivalTimes(){
+        return new ArrayList<>(Arrays.asList(converter.transform("08:23"), converter.transform("08:26")));
     }
 }
